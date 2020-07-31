ef9c48835f3966b67e68ce5a8d6f7a3033780d7d
Jack Haynes <jackhaynes94@gmail.com>
2020-07-31 20:23:57 +0100

    Created EmailAddress, Event, Attendee and Organiser domain entities.

--- src/main/java/domain/Event.java
+++ src/main/java/domain/Event.java
@@ -1,4 +1,15 @@
-package domain;
+package domain.event;
+
+import domain.Attendee;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
 
 public class Event {
+    private String name;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String description;
+
+    private Collection<Attendee> attendees;
 }
